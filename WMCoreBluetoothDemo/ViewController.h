828266a7e1eeab6b40e120d1e87e1a315146a63f
Maginawin <514763292@qq.com>
2015-03-11 20:08:07 +0800

    Initial Commit

---
+++ WMCoreBluetoothDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMCoreBluetoothDemo
+//
+//  Created by wangwendong on 15/3/11.
+//  Copyright (c) 2015年 WM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
